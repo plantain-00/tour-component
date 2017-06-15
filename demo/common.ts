b8ec0f70cd37aa1e56489e444f0c30196cc8af83
york yao <yaoao12306@outlook.com>
2017-06-15 19:13:02 +0800

    better bundle config

--- demo/common.ts
+++ demo/common.ts
@@ -5,7 +5,7 @@ export const data: common.TourData = {
         { left: "100px", top: "50px", direction: "top", content: "step 1", next: "next" },
         { left: "200px", top: "100px", direction: "bottom", content: "step 2", next: "next" },
         { right: "300px", top: "150px", direction: "left", content: "step 3", next: "next" },
-        { right: "300px", top: "200px", direction: "right", content: "step 4", next: "done" },
+        { right: "300px", top: "500px", direction: "right", content: "step 4", next: "done" ,scrollTop: 500 },
     ],
     localStorageKey: "tour-test",
 };
--- src/common.ts
+++ src/common.ts
@@ -11,4 +11,49 @@ export type Step = {
     direction: "left" | "right" | "top" | "bottom";
     content: string;
     next: string;
+    scrollTop?: number;
 };
+
+export function scrollToY(scrollTargetY = 0, speed = 2000, easing: "easeOutSine" | "easeInOutSine" | "easeInOutQuint" = "easeOutSine") {
+    const scrollY = window.scrollY || document.documentElement.scrollTop;
+    let currentTime = 0;
+    const time = Math.max(.1, Math.min(Math.abs(scrollY - scrollTargetY) / speed, 0.8));
+    const easingEquations: { [easing: string]: (pos: number) => number } = {
+        easeOutSine: (pos: number) => {
+            return Math.sin(pos * (Math.PI / 2));
+        },
+        easeInOutSine: (pos: number) => {
+            return (-0.5 * (Math.cos(Math.PI * pos) - 1));
+        },
+        easeInOutQuint: (pos: number) => {
+            pos /= 0.5;
+            if (pos < 1) {
+                return 0.5 * Math.pow(pos, 5);
+            }
+            return 0.5 * (Math.pow((pos - 2), 5) + 2);
+        },
+    };
+
+    function tick() {
+        currentTime += 1 / 60;
+
+        const p = currentTime / time;
+        const t = easingEquations[easing](p);
+
+        if (p < 1) {
+            (window.requestAnimationFrame ||
+                window.webkitRequestAnimationFrame ||
+                (window as any).mozRequestAnimationFrame ||
+                (callback => {
+                    window.setTimeout(callback, 1000 / 60);
+                })
+            )(tick);
+
+            window.scrollTo(0, scrollY + ((scrollTargetY - scrollY) * t));
+        } else {
+            window.scrollTo(0, scrollTargetY);
+        }
+    }
+
+    tick();
+}
