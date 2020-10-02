f893deaf7e3300b45c014756d0ff37f7fbe32faf
York Yao <me@yorkyao.com>
2020-10-02 17:53:51 +0800

    feat: support vue3

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -1,63 +1,71 @@
-import Vue from 'vue'
-import Component from 'vue-class-component'
+import { defineComponent, PropType } from 'vue'
 import * as common from 'tour-component'
 export * from 'tour-component'
-import { indexTemplateHtml, indexTemplateHtmlStatic } from './variables'
+import { indexTemplateHtml } from './variables'
 
-@Component({
+/**
+ * @public
+ */
+export const Tour = defineComponent({
   render: indexTemplateHtml,
-  staticRenderFns: indexTemplateHtmlStatic,
-  props: ['data']
-})
-export class Tour extends Vue {
-  data!: common.TourData
-  private index = 0
-
-  get step() {
-    return (this.index < this.data.steps.length && this.index >= 0) ? this.data.steps[this.index] : null
-  }
-  get arrowClassName() {
-    return this.step ? `tour-arrow tt-${this.step.direction}` : 'tour-arrow'
-  }
-  get position() {
-    return common.getStepPosition(this.step)
-  }
-
+  props: {
+    data: {
+      type: Object as PropType<common.TourData>,
+      required: true,
+    },
+  },
+  data: () => {
+    return {
+      index: 0,
+    }
+  },
+  computed: {
+    step(): common.Step | null {
+      return (this.index < this.data.steps.length && this.index >= 0) ? this.data.steps[this.index] : null
+    },
+    arrowClassName(): string {
+      return this.step ? `tour-arrow tt-${this.step.direction}` : 'tour-arrow'
+    },
+    position(): {
+      left?: string;
+      right?: string;
+      top?: string;
+      bottom?: string;
+    } {
+      return common.getStepPosition(this.step)
+    },
+  },
   beforeMount() {
     this.index = this.data.index
-  }
-
+  },
   mounted() {
     this.highlight()
-  }
-
-  next() {
-    this.index++
-    this.$emit('update', this.index)
-    this.highlight()
-    if (this.index >= this.data.steps.length) {
-      this.close()
-    }
-  }
-
-  close() {
-    this.index = -1
-    this.$emit('update', this.index)
-    this.highlight()
-    if (this.data.localStorageKey) {
-      localStorage.setItem(this.data.localStorageKey, '1')
-    }
-  }
-
-  private highlight() {
-    common.unhighlight(this.data.steps)
-    if (this.step) {
-      common.highlight(this.step)
-      if (typeof this.step.scrollTop === 'number') {
-        window.scrollTo(0, this.step.scrollTop)
+  },
+  methods: {
+    next() {
+      this.index++
+      this.$emit('update', this.index)
+      this.highlight()
+      if (this.index >= this.data.steps.length) {
+        this.close()
       }
-    }
+    },
+    close() {
+      this.index = -1
+      this.$emit('update', this.index)
+      this.highlight()
+      if (this.data.localStorageKey) {
+        localStorage.setItem(this.data.localStorageKey, '1')
+      }
+    },
+    highlight() {
+      common.unhighlight(this.data.steps)
+      if (this.step) {
+        common.highlight(this.step)
+        if (typeof this.step.scrollTop === 'number') {
+          window.scrollTo(0, this.step.scrollTop)
+        }
+      }
+    },
   }
-}
-
-Vue.component('tour', Tour)
+})
